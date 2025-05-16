9cbc0943a9641be049e2b5cbc4b29e971f2a1261
Milan <hanzalikm@gmail.com>
2025-05-17 01:10:20 +0200

    chore: create simple quick start script for google search

--- quickstart.py
+++ quickstart.py
@@ -1,57 +1,36 @@
-import os
-import psycopg2
-from dotenv import load_dotenv
+import json
 from serpapi import GoogleSearch
 
 # Paramery pre google_search - crawler
 params = {
   "api_key": "5a4878198e466dfb74b91e3977434899026e471c42fbd14b7919451ab05d0582",
   "engine": "google",
-  "q": "Gasotech, s.r.o.",
+  "q": "MC POWER GROUP, s. r. o.",
   "google_domain": "google.sk",
   "gl": "sk",
   "hl": "sk",
   "uule": "Slovakia"
 }
 
-# Načíta premenné z .env súboru
-load_dotenv()
+search = GoogleSearch(params)
+results = search.get_dict()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-def main():
-    try:
-        conn = psycopg2.connect(
-            host=DB_HOST,
-            port=DB_PORT,
-            dbname=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD
-        )
-        cur = conn.cursor()
+# Primárne: knowledge_graph
+website = None
+if "knowledge_graph" in results and "website" in results["knowledge_graph"]:
+    website = results["knowledge_graph"]["website"]
+# Fallback: organic_results
+elif "organic_results" in results and results["organic_results"]:
+    website = results["organic_results"][0].get("link")
 
-        cur.execute("SELECT nazovfirmy, kraj FROM firmy WHERE kraj = 'Bratislavský';")
-        rows = cur.fetchall()
+# Výpis a uloženie
+print("Nájdená stránka:", website if website else "[nenájdená]")
 
-        print("Zoznam firiem:")
-        for row in rows:
-            print(f"- {row[0]} ({row[1]})")
+print(results)
 
-        cur.close()
-        conn.close()
-    except Exception as e:
-        print(f"Chyba pri práci s databázou: {e}")
+# Ulož do súboru results.json
+with open("googlesearch_results.json", "w", encoding="utf-8") as f:
+    json.dump(results, f, ensure_ascii=False, indent=2)
 
-if __name__ == "__main__":
-    main()
-
-#Clawler
-
-# search = GoogleSearch(params)
-# results = search.get_dict()
-
-# print(results['knowledge_graph']['website'])
+print("✅ Výsledky uložené do results.json")
